c08b0fdde19623869482512d10cdfdb99a8eb290
koyeo <koyeo@qq.com>
2021-07-06 14:41:32 +0800

    update test

--- engine_test.go
+++ engine_test.go
@@ -33,9 +33,6 @@ func rv(v interface{}) reflect.Value {
 
 func TestEngine(t *testing.T) {
 	
-	// TODO 支持手动调用 SQL 语句
-	//db.Query().Parameter("", 123, 34, 5).Result("name, age", &name, &age)()
-	
 	engine := NewPostgresql("postgresql://postgres:postgres@127.0.0.1:5432/gobatis?connect_timeout=10&sslmode=disable")
 	err := engine.Init(NewBundle("test"))
 	require.NoError(t, err)
