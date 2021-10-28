a8527f0551b75064cbae9a0ee327186c75aec818
alpha <alphaqiu@gmail.com>
2021-10-28 14:24:07 +0800

    support ssh tunnel

--- driver/mysql/db.go
+++ driver/mysql/db.go
@@ -1,14 +1,23 @@
 package mysql
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/ssh"
+	"net"
 )
 
 const MySQL = "mysql"
 
-func InitDB(dsn string) (db *sql.DB, err error) {
+func InitDB(dsn string, sshClient *ssh.Client) (db *sql.DB, err error) {
+	if sshClient != nil {
+		mysql.RegisterDialContext("tcp", func(ctx context.Context, addr string) (net.Conn, error) {
+			return sshClient.Dial("tcp", addr)
+		})
+	}
+
 	db, err = sql.Open(MySQL, dsn)
 	if err != nil {
 		err = fmt.Errorf("mysql connnet error: %s", err)
--- driver/postgresql/db.go
+++ driver/postgresql/db.go
@@ -1,18 +1,46 @@
 package postgresql
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/stdlib"
+	"golang.org/x/crypto/ssh"
+	"net"
+	"regexp"
 )
 
 const PGX = "pgx"
 
-func InitDB(dsn string) (db *sql.DB, err error) {
-	db, err = sql.Open(PGX, dsn)
-	if err != nil {
-		err = fmt.Errorf("postgresql connnet error: %s", err)
+func InitDB(dsn string, sshClient *ssh.Client) (db *sql.DB, err error) {
+	if sshClient == nil {
+		db, err = sql.Open(PGX, dsn)
+		if err != nil {
+			err = fmt.Errorf("postgresql connnet error: %s", err)
+			return
+		}
 		return
+	} else {
+		var config *pgx.ConnConfig
+
+		config, err = pgx.ParseConfig(dsn)
+		if err != nil {
+			return nil, err
+		}
+
+		config.PreferSimpleProtocol = true
+		result := regexp.MustCompile("(time_zone|TimeZone)=(.*?)($|&| )").FindStringSubmatch(dsn)
+		if len(result) > 2 {
+			config.RuntimeParams["timezone"] = result[2]
+		}
+
+		config.DialFunc = func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return sshClient.Dial(network, addr)
+		}
+
+		db = stdlib.OpenDB(*config)
+		return db, nil
 	}
-	return
+
 }
