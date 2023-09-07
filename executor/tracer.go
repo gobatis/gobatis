1525162dc6d4a6dbe5fb6ac113da3c12b0253a1a
koyeo <guyue.me@gmail.com>
2023-09-07 23:34:23 +0800

    update tracer

--- executor/tracer.go
+++ executor/tracer.go
@@ -9,15 +9,15 @@ import (
 	"github.com/gozelle/color"
 )
 
-type Trace struct {
+type Tracer struct {
 	Error   error
 	debug   bool
 	traceId string
-	tx      interface{}
+	tx      bool
 	Logger  Logger
 }
 
-func (db *Trace) log() {
+func (db Tracer) log() {
 	if !db.debug && db.Error == nil {
 		return
 	}
@@ -37,7 +37,7 @@ func (db *Trace) log() {
 		traceId = fmt.Sprintf("[%s]", color.CyanString(db.traceId))
 	}
 	var t string
-	if db.tx != nil {
+	if db.tx {
 		t = fmt.Sprintf("[%s]", color.CyanString("Tx"))
 	}
 	info.WriteString(fmt.Sprintf("%s %s", color.MagentaString("[gobatis]"), color.RedString(db.runFuncPos(4))))
@@ -50,7 +50,7 @@ func (db *Trace) log() {
 
 // runFuncPos returns the file name and line number of the caller of the function calling it.
 // skip: 0 for the current function, 1 for the caller of the current function
-func (db *Trace) runFuncPos(skip int) string {
+func (db Tracer) runFuncPos(skip int) string {
 	i := skip
 	for {
 		_, file, line, ok := runtime.Caller(i)
